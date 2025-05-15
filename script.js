de592782fef88dac963155e6255742c32dce98a2
leei006 <leei006@newschool.edu>
2025-05-15 18:43:23 -0400

    pushing new code

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ function changeImage() {
 
 // Change image every 2 seconds
 setInterval(changeImage, 2000);
+
+const slideshowImages = [];
+
+for (let i = 1; i <= 27; i++) {
+  slideshowImages.push(`img/gardeningbook${i}.jpg`);
+}
+
+let currentSlide = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slideshowImg = document.getElementById('slideshow-img');
+
+  if (slideshowImg) {
+    slideshowImg.addEventListener('click', () => {
+      currentSlide = (currentSlide + 1) % slideshowImages.length;
+      slideshowImg.src = slideshowImages[currentSlide];
+    });
+  }
+});
+
+const slideshow = document.getElementById('slideshow');
+const tooltip = document.getElementById('cursorTooltip');
+
+slideshow.addEventListener('mousemove', (e) => {
+  tooltip.style.opacity = '1';
+  tooltip.style.left = e.clientX + 15 + 'px'; // 15px offset from cursor
+  tooltip.style.top = e.clientY + 15 + 'px';
+});
+
+slideshow.addEventListener('mouseleave', () => {
+  tooltip.style.opacity = '0';
+});
