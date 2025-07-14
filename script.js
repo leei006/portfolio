9e674c37ee66107e2d86d8708e73a46969509cff
leei006 <leei006@newschool.edu>
2025-07-14 13:22:07 -0400

    bug error

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
   setupClickthrough('slideshow2', 'slideshow-img2', 'img/diningshed', 26);
   setupClickthrough('slideshow3', 'slideshow-img3', 'img/groove', 1);
   setupClickthrough('slideshow4', 'slideshow-img4', 'img/destijl', 3); 
+  setupClickthrough('slideshow5', 'slideshow-img5', 'img/dogcarecampaign', 1); 
 
   // Load images dynamically based on page
   const gallery = document.getElementById('gallery');
