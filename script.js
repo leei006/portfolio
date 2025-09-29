d6c3b1440d8e0f807132d4bbfc71c99ade7a602b
leei006 <leei006@newschool.edu>
2025-09-29 16:47:36 -0400

    updates to portfolio

--- script.js
+++ script.js
@@ -14,26 +14,47 @@ const photo = [
     "img":["img/bench2.jpg", "img/bench3.jpg", "img/bench4.jpg", "img/bench6.jpg", "img/bench7.jpg", "img/bench8.jpg", "img/bench9.jpg", "img/bench11.jpg", "img/bench12.jpg"]
   },
   {
+    "id": 12,
+    "title": "Wiki Book",
+    "year": 2025,
+    "medium": "Graphic Design",
+    "img": [
+      // "img/wiki1.jpg",
+      "img/wiki12.jpg",
+      "img/wiki2.jpg",
+      "img/wiki3.jpg",
+      "img/wiki4.jpg",
+      "img/wiki5.jpg",
+      "img/wiki6.jpg",
+      "img/wiki7.jpg",
+      "img/wiki8.jpg",
+      "img/wiki9.jpg",
+      "img/wiki10.jpg",
+      "img/wiki11.jpg",
+    ]
+  },
+  {
     "id": 1,
     "title": "De Stijl",
     "year": 2023,
     "medium": "Typographic print in color on copy paper.",
-    "img":["img/destijl3.jpg", "img/destijl1.jpg", "img/destijl2.jpg"]
+    "img":["img/vilmos1.jpg", "img/vilmos2.jpg", "img/vilmos3.jpg"]
   },
-  {
-    "id": 7,
-    "title": "Groove",
-    "year": 2024,
-    "medium": "Typeface Design. Inspired by shapes defining the 1960's.",
-    "img":["img/groove1.png"]
-  },
+
+  // {
+  //   "id": 7,
+  //   "title": "Groove",
+  //   "year": 2024,
+  //   "medium": "Typeface Design. Inspired by shapes defining the 1960's.",
+  //   "img":["img/groove1.png"]
+  // },
   {
     "id": 2,
     "title": "Dining Shed Collection",
     "year": 2024,
     "medium": "City collection of pandemic dining sheds.",
     "img": [
-      "img/collectionphotos1.jpg","img/collectionphotos2.jpg","img/collectionphotos3.jpg","img/collectionphotos4.jpg","img/collectionphotos5.jpg","img/collectionphotos6.jpg","img/collectionphotos7.jpg","img/collectionphotos8.jpg","img/collectionphotos9.jpg","img/collectionphotos10.jpg","img/collectionphotos11.jpg","img/collectionphotos12.jpg","img/collectionphotos13.jpg","img/collectionphotos14.jpg","img/collectionphotos15.jpg","img/collectionphotos16.jpg","img/collectionphotos17.jpg","img/collectionphotos18.jpg","img/collectionphotos19.jpg","img/collectionphotos20.jpg","img/collectionphotos21.jpg","img/collectionphotos22.jpg","img/collectionphotos23.jpg","img/collectionphotos24.jpg","img/collectionphotos25.jpg","img/collectionphotos26.jpg","img/collectionphotos27.jpg","img/collectionphotos28.jpg","img/collectionphotos29.jpg"
+      "img/collectionphotos3.jpg","img/collectionphotos1.jpg","img/collectionphotos2.jpg","img/collectionphotos4.jpg","img/collectionphotos5.jpg","img/collectionphotos6.jpg","img/collectionphotos7.jpg","img/collectionphotos8.jpg","img/collectionphotos9.jpg","img/collectionphotos10.jpg","img/collectionphotos11.jpg","img/collectionphotos12.jpg","img/collectionphotos13.jpg","img/collectionphotos14.jpg","img/collectionphotos15.jpg","img/collectionphotos16.jpg","img/collectionphotos17.jpg","img/collectionphotos18.jpg","img/collectionphotos19.jpg","img/collectionphotos20.jpg","img/collectionphotos21.jpg","img/collectionphotos22.jpg","img/collectionphotos23.jpg","img/collectionphotos24.jpg","img/collectionphotos25.jpg","img/collectionphotos26.jpg","img/collectionphotos27.jpg","img/collectionphotos28.jpg","img/collectionphotos29.jpg"
     ]
   },
   {
@@ -50,20 +71,20 @@ const photo = [
     "medium": "Photography. San Francisco Bay",
     "img": ["img/homeshoot1.jpg", "img/homeshoot2.jpg", "img/homeshoot3.jpg","img/homeshoot4.jpg","img/homeshoot5.jpg"]
   },
-  {
-    "id": 5,
-    "title": "Dining Shed Book",
-    "year": 2024,
-    "medium": "Perfect bound color printed book documenting pandemic dining sheds.",
-    "img":["img/diningshed1.jpg","img/diningshed2.jpg","img/diningshed3.jpg","img/diningshed4.jpg","img/diningshed5.jpg","img/diningshed6.jpg","img/diningshed7.jpg","img/diningshed8.jpg","img/diningshed9.jpg","img/diningshed10.jpg","img/diningshed11.jpg","img/diningshed12.jpg","img/diningshed13.jpg","img/diningshed14.jpg","img/diningshed15.jpg","img/diningshed16.jpg","img/diningshed17.jpg","img/diningshed18.jpg","img/diningshed19.jpg","img/diningshed20.jpg","img/diningshed21.jpg","img/diningshed22.jpg","img/diningshed23.jpg","img/diningshed24.jpg","img/diningshed25.jpg","img/diningshed26.jpg"]
-  },
-  {
-    "id": 6,
-    "title": "Vegetable",
-    "year": 2025,
-    "medium": "Photography. La Plaza, New York",
-    "img": ["img/6.jpg", "img/7.jpg"]
-  },
+  // {
+  //   "id": 5,
+  //   "title": "Dining Shed Book",
+  //   "year": 2024,
+  //   "medium": "Perfect bound color printed book documenting pandemic dining sheds.",
+  //   "img":["img/diningshed1.jpg","img/diningshed2.jpg","img/diningshed3.jpg","img/diningshed4.jpg","img/diningshed5.jpg","img/diningshed6.jpg","img/diningshed7.jpg","img/diningshed8.jpg","img/diningshed9.jpg","img/diningshed10.jpg","img/diningshed11.jpg","img/diningshed12.jpg","img/diningshed13.jpg","img/diningshed14.jpg","img/diningshed15.jpg","img/diningshed16.jpg","img/diningshed17.jpg","img/diningshed18.jpg","img/diningshed19.jpg","img/diningshed20.jpg","img/diningshed21.jpg","img/diningshed22.jpg","img/diningshed23.jpg","img/diningshed24.jpg","img/diningshed25.jpg","img/diningshed26.jpg"]
+  // },
+  // {
+  //   "id": 6,
+  //   "title": "Vegetable",
+  //   "year": 2025,
+  //   "medium": "Photography. La Plaza, New York",
+  //   "img": ["img/6.jpg", "img/7.jpg"]
+  // },
   {
     "id": 9,
     "title": "Food",
